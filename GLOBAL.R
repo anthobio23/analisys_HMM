a8e78e2dddfac408829668ca236d31814792f29f
anthobio23 <anthonyservita05@gmail.com>
2022-05-31 23:02:51 -0500

    proyecto sin terminar

---
+++ GLOBAL.R
@@ -0,0 +1,7 @@
+if (!require(readxl) == TRUE) {install.packages("readxl")}
+if (!require(dplyr) == TRUE) {install.packages("dplyr")}
+if (!require(tidyverse) == TRUE) {install.packages("tidyverse")}
+if (!require(markovchain) == TRUE) {install.packages("markovchain")}
+if (!require(reshipe) == TRUE) {install.packages("reshape")}
+if (!require(patchwork) == TRUE) {install.packages("patchwork")}
+if (!require(hrbrthemes) == TRUE) {install.packages("hrbrthemes")}
